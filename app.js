02c94600c62b8901fcd3f325dfff6cbffa2c4ad7
chabid492 <chabid492@gmail.com>
2022-08-19 00:29:16 +0500

    halmet issue fixed on console error content-type, movie detail page and search page working

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const helmet=require('helmet');
+var cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,7 +17,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(helmet());
+app.use(cors())
+//app.use(helmet()); only use when api calls and use in route files
+
+//following middleware will use when external resource will use in our app
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "img-src": ["'self'", "https: data:"]
+    }
+  })
+)
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,61 @@
 var express = require('express');
 var router = express.Router();
 
+// Get the default instance
+const axios = require('axios')
+
+const apiKey='1fb720b97cc13e580c2c35e1138f90f8';
+
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
 /* GET home page. */
+//set imageBaseUrl as global to whole application
+router.use((req,res,next) => {
+    res.locals.imageBaseUrl=imageBaseUrl;
+    next();
+});
+
+//following middleware is use to solve content policy errors in console
+router.use((req, res, next) => {
+  res.set("Content-Security-Policy", "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'");
+  next();
+})
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // Make the GET call by passing a config object to the instance
+  axios.get(nowPlayingUrl).then(apiRes => {
+    // process the response
+     var data = apiRes.data.results;
+     //console.log(data);
+     //res.json(data);
+     res.render('index',{data:data});
+  });
+
+  //res.render('index', {  });
+});
+
+router.get('/movie/:id',(req,res,next)=>{
+  const movieId=req.params.id;
+  const url=`${ apiBaseUrl }/movie/${ movieId }?api_key=${ apiKey }`;
+  axios.get(url).then(apiRes=>{
+    const data=apiRes.data;
+    res.render('detail',{data:data});
+  });
+  //res.send(movieId);
+});
+
+router.post('/search',(req,res,next)=>{
+  const search=encodeURI(req.body.movieSearch);
+  const cat=req.body.cat;
+  const url=`${ apiBaseUrl }/search/${ cat }?query=${ search }&api_key=${ apiKey }`;
+  axios.get(url).then(apiRes=>{
+    const data=apiRes.data.results;
+    //res.json(data);
+    res.render('index',{data:data});
+  });
+  //res.send(movieId);
 });
 
 module.exports = router;
